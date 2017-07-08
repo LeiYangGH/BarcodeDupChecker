0e58e9cd81b70a7e4e20abb1457b315237daf040
Lei, Yang <yang_lei@mcafee.com>
2017-07-08 08:41:52 +0800

    alarm sp test ok

---
+++ BarcodeDupChecker/TestSPWrite/SerialPortAccessor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestSPWrite
+{
+    public class SerialPortAccessor
+    {
+        private static readonly Lazy<SerialPortAccessor> lazy =
+            new Lazy<SerialPortAccessor>(() => new SerialPortAccessor());
+        public static SerialPortAccessor Instance { get { return lazy.Value; } }
+
+        static SerialPort serialPort = new SerialPort();
+
+
+        private SerialPortAccessor()
+        {
+            try
+            {
+                serialPort.PortName = "COM3";//修改要读的串口名字
+                serialPort.BaudRate = 9600;//波特率
+                //Parity p = serialPort.Parity;
+                //serialPort.Parity = Parity.None;
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void SendBytes(byte[] bytes)
+        {
+            try
+            {
+                serialPort.Write(bytes,0,bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //public void SendText(string text)
+        //{
+        //    try
+        //    {
+        //        serialPort.Write(text);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+
+    }
+    }
---
+++ BarcodeDupChecker/TestSPWrite/UCBytes.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSPWrite
+{
+    /// <summary>
+    /// UCBytes.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCBytes : UserControl
+    {
+        public UCBytes()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string BytesText
+        {
+            get { return (string)GetValue(BytesTextProperty); }
+            set { SetValue(BytesTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Property1.  
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BytesTextProperty
+            = DependencyProperty.Register(
+                  "BytesText",
+                  typeof(string),
+                  typeof(UCBytes),
+                  new PropertyMetadata("")
+              );
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(this.BytesText);
+            btn.IsEnabled = false;
+            byte[] bytes = StringToByteArray(this.BytesText);
+            SerialPortAccessor.Instance.SendBytes(bytes);
+
+            btn.IsEnabled = true;
+        }
+
+        private byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+    }
+}
